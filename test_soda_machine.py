7806a1913dcfb67b3f98099074ac707ff9a1412e
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-10-13 17:49:24 -0400

    Finished 4. register_has_coin - 5 tests a. Test that each type of coin will return True b Test a non valid coin

--- test_soda_machine.py
+++ test_soda_machine.py
@@ -3,10 +3,9 @@ from soda_machine import SodaMachine
 
 
 class TestFillRegister(unittest.TestCase):
-    """tests that register list holds 88 coins"""
+    """test that register list has a len of 88"""
 
     def setUp(self):
-        """Sets up testing environment """
         self.soda_machine = SodaMachine()
     
 
@@ -14,27 +13,40 @@ class TestFillRegister(unittest.TestCase):
         """Checking to see if list length is 88"""
         self.assertEqual(len(self.soda_machine.register), 88)
 
+class TestFillInventory(unittest.TestCase):
+    """test that its inventory list has a len of 30"""
+
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+
     def test_inventory_list(self):
         """Checking to see if list length is 30"""
         self.assertEqual(len(self.soda_machine.inventory), 30)
 
+class TestGetCoinFromRegister(unittest.TestCase):
+    """Test each type of coin can be returned from register"""
+
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
     def test_get_coin_quarter(self):
-        """Checking each coin type can be returned from register"""
+        """Checking quarter coin type can be returned from register"""
         returned_coin = self.soda_machine.get_coin_from_register('Quarter')
         self.assertEqual(returned_coin.value, .25)
 
     def test_get_coin_dime(self):
-        """Checking each coin type can be returned from register"""
+        """Checking dime coin type can be returned from register"""
         returned_coin = self.soda_machine.get_coin_from_register('Dime')
         self.assertEqual(returned_coin.value, .10)
 
     def test_get_coin_nickel(self):
-        """Checking each coin type can be returned from register"""
+        """Checking nickel coin type can be returned from register"""
         returned_coin = self.soda_machine.get_coin_from_register('Nickel')
         self.assertEqual(returned_coin.value, .05)
 
     def test_get_coin_penny(self):
-        """Checking each coin type can be returned from register"""
+        """Checking penny coin type can be returned from register"""
         returned_coin = self.soda_machine.get_coin_from_register('Penny')
         self.assertEqual(returned_coin.value, .01)
 
@@ -42,6 +54,37 @@ class TestFillRegister(unittest.TestCase):
         """Checking to pass in a string that is not a valid coin name will return None"""
         returned_coin = self.soda_machine.get_coin_from_register('Yen')
         self.assertEqual(returned_coin, None)
+
+class TestRegisterHasCoin(unittest.TestCase):
+    """Testing coin names in register are True or False"""
+
+    def setUp(self):
+        self.soda_machine = SodaMachine()  
+
+    def test_coin_true_quarter(self):
+        """Checking that Quarter type of coin will return True"""
+        returned_coin = self.soda_machine.register_has_coin('Quarter')
+        self.assertEqual(returned_coin, True)
+
+    def test_coin_true_dime(self):
+        """Checking that Dime type of coin will return True"""
+        returned_coin = self.soda_machine.register_has_coin('Dime')
+        self.assertEqual(returned_coin, True)
+
+    def test_coin_true_nickel(self):
+        """Checking that Nickel type of coin will return True"""
+        returned_coin = self.soda_machine.register_has_coin('Nickel')
+        self.assertEqual(returned_coin, True)
+
+    def test_coin_true_penny(self):
+        """Checking that Penny type of coin will return True"""
+        returned_coin = self.soda_machine.register_has_coin('Penny')
+        self.assertEqual(returned_coin, True)
+
+    def test_coin_false(self):
+        """Checking that a non-valid coin name will return False"""
+        returned_coin = self.soda_machine.register_has_coin('Yen')
+        self.assertFalse(returned_coin, False)
 
 if __name__ == '__main__':
     unittest.main()

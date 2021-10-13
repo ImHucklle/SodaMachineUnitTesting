b3cdf88a6768d1c41e6eec8cc7e649ab681134c1
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-10-13 12:44:31 -0400

    Added the test customer file and the methods for all coins

---
+++ test_customer.py
@@ -0,0 +1,38 @@
+import unittest 
+from customer import Customer 
+
+
+class TestGetWalletCoin(unittest.TestCase):
+    """Test for customers get_wallet_coin method"""
+
+    def setUp(self):
+        self.customer = Customer()
+
+    def test_can_return_quarter(self):
+        """Pass in 'Quarter' method should return a Quarter instance"""
+        returned_coin = self.customer.get_wallet_coin('Quarter')
+        self.assertEqual(returned_coin.value, .25)
+
+    def test_can_return_dime(self):
+        """Pass in 'Dime' method should return a Dime instance"""
+        returned_coin = self.customer.get_wallet_coin('Dime')
+        self.assertEqual(returned_coin.value, .10)
+
+    def test_can_return_nickel(self):
+        """Pass in 'Nickel' method should return a Nickel instance"""
+        returned_coin = self.customer.get_wallet_coin('Nickel')
+        self.assertEqual(returned_coin.value, .05)
+
+    def test_can_return_penny(self):
+        """Pass in 'Penny' method should return a Penny instance"""
+        returned_coin = self.customer.get_wallet_coin('Penny')
+        self.assertEqual(returned_coin.value, .01)
+
+    def test_can_return_none(self):
+        """Pass in a string that is non a valued coin to return none"""
+        
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
